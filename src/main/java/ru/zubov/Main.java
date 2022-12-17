21827804bae906ffd856c6b1ca8cb7ffd16b2296
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-12-17 23:49:30 +0300

    Correct log4j and add changelog

--- src/main/java/ru/zubov/Main.java
+++ src/main/java/ru/zubov/Main.java
@@ -1,11 +1,17 @@
 package ru.zubov;
 
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
 import ru.zubov.entity.User;
 import ru.zubov.utils.HibernateUtil;
 
+@Log4j2
 public class Main {
+
+
     public static void main(String[] args) {
+        log.info("Start Hibernate method!");
+
         Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.getTransaction().begin();
