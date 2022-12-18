a00575ed7a1fed9d95bd8adb304219cc761a325a
Vitaly Zubov <Zubov.VP@yandex.ru>
2022-12-18 23:49:12 +0300

    Learn Criteria API

--- src/main/java/ru/zubov/Main.java
+++ src/main/java/ru/zubov/Main.java
@@ -1,9 +1,12 @@
 package ru.zubov;
 
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.*;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
 import ru.zubov.entity.User;
 import ru.zubov.utils.HibernateUtil;
+
 
 @Log4j2
 public class Main {
@@ -16,12 +19,27 @@ public class Main {
 
         session.getTransaction().begin();
 
-        User user = new User();
-        user.setEmail("newEmail@gmail.com");
-        user.setUsername("User");
-        user.setPassword("123456");
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
 
-        session.persist(user);
+        //
+
+        //Запрос с несколькими параметрами
+//        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+//        Root<User> root = criteriaQuery.from(User.class);
+//        criteriaQuery.select(root);
+//        Predicate p1 = criteriaBuilder.gt(root.get("id"), 20);
+//        Predicate p2 = criteriaBuilder.lt(root.get("id"), 22);
+//        criteriaQuery.select(root).where(criteriaBuilder.and(p1, p2));
+//        Query query = session.createQuery(criteriaQuery);
+//        List<User> users = query.getResultList();
+
+
+        //Удаление записи по id
+        CriteriaDelete<User> criteriaDelete = criteriaBuilder.createCriteriaDelete(User.class);
+        Root<User> root = criteriaDelete.from(User.class);
+        criteriaDelete.where(criteriaBuilder.equal(root.get("id"), 15));
+        session.createQuery(criteriaDelete).executeUpdate();
+
 
         session.getTransaction().commit();
         session.close();
