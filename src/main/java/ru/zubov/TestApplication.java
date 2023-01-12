1fefe6fa726d1cbf96eec84277540015bd830fc8
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-01-12 23:43:56 +0300

    Create test application (create priority and category)

--- src/main/java/ru/zubov/TestApplication.java
+++ src/main/java/ru/zubov/TestApplication.java
@@ -1,8 +1,14 @@
 package ru.zubov;
 
 import lombok.extern.log4j.Log4j2;
+import ru.zubov.dao.impl.CategoryDAOImpl;
+import ru.zubov.dao.impl.PriorityDaoImpl;
 import ru.zubov.dao.impl.UserDaoCriteria;
 import ru.zubov.dao.interfaces.CrudDao;
+import ru.zubov.dao.interfaces.objects.CategoryDao;
+import ru.zubov.dao.interfaces.objects.PriorityDao;
+import ru.zubov.entity.Category;
+import ru.zubov.entity.Priority;
 import ru.zubov.entity.User;
 import ru.zubov.utils.HibernateUtil;
 
@@ -10,22 +16,37 @@ import ru.zubov.utils.HibernateUtil;
 public class TestApplication {
 
     public static void main(String[] args) {
+        //Тестирование работы приложения
 
         CrudDao<User> userDao = new UserDaoCriteria();
-        //Создаём пользователя
+        //1) Создаём пользователя
 //        User user = new User();
 //        user.setUsername("Mike");
 //        user.setPassword("111");
 //        user.setEmail("mike@mail.com");
 //        userDao.add(user);
+        User user = userDao.get(30L);
 
-        //Активируем пользователя
+        //2) Активируем пользователя
 //        ActivityDao activityDao = new ActivityDaoImpl();
 //        Activity activity =  activityDao.getByUser(user);
 //        activity.setActivatied(true);
 //        activityDao.update(activity);
 
-        User user = userDao.get(30L);
+//      3) Создание новой категории
+        CategoryDao categoryDao = new CategoryDAOImpl();
+        Category newCategory = new Category();
+        newCategory.setTitle("Новая категория");
+        newCategory.setUser(user);
+        categoryDao.add(newCategory);
+
+//      4) Создание нового приоритета
+        PriorityDao priorityDao = new PriorityDaoImpl();
+        Priority newPriority = new Priority();
+        newPriority.setTitle("Новый приоритет");
+        newPriority.setColor("white");
+        newPriority.setUser(user);
+        priorityDao.add(newPriority);
 
 
         //Добавление статистики по использованию L2C
