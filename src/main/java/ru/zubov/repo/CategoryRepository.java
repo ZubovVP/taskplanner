9a144d03e2cb0467ef6d36ab790652938a992ec3
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-03-16 23:48:26 +0300

    Add findByUserEmailOrderByTitleAsc and test

--- src/main/java/ru/zubov/repo/CategoryRepository.java
+++ src/main/java/ru/zubov/repo/CategoryRepository.java
@@ -4,7 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.zubov.entity.Category;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+
+    /**
+     * Поиск категорий пользователя (по email) сортировка по названию
+     * @param email - email пользователя
+     * @return лист списка категорий в отсортированном виде
+     */
+    List<Category> findByUserEmailOrderByTitleAsc(String email);
 }
