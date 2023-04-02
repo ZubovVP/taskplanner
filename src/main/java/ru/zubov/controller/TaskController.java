50f6048f57609bad1342e0ac529941eb05226ef3
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-04-02 23:47:49 +0300

    Add basic methods for task

--- src/main/java/ru/zubov/controller/TaskController.java
+++ src/main/java/ru/zubov/controller/TaskController.java
@@ -1,12 +1,14 @@
 package ru.zubov.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.zubov.entity.Task;
 import ru.zubov.service.TaskService;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/task")
@@ -14,8 +16,56 @@ import ru.zubov.service.TaskService;
 public class TaskController {
     private final TaskService taskService;
 
-    @PostMapping("/findById")
-    public Task findById(@RequestParam Long id) {
-        return taskService.findById(id).orElse(null);
+    @PutMapping("/add")
+    public ResponseEntity<Task> add(@RequestBody Task task) {
+        if (task.getId() == null || task.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (task.getTitle() == null || task.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        taskService.update(task);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Task> update(@RequestBody Task task) {
+        if (task.getId() == null || task.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (task.getTitle() == null || task.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        taskService.update(task);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        try {
+            taskService.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity findById(@RequestParam Long id) {
+        Task task;
+        try {
+            task = taskService.findById(id).orElse(null);
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return new ResponseEntity("id=" + id + " not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(task);
     }
 }
--- src/main/java/ru/zubov/service/TaskService.java
+++ src/main/java/ru/zubov/service/TaskService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import ru.zubov.entity.Task;
 import ru.zubov.repo.TaskRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,4 +18,23 @@ public class TaskService {
     public Optional<Task> findById(Long id) {
         return taskRepository.findById(id);
     }
+
+    public List<Task> findAll(String email) {
+        return taskRepository.findByUserEmailOrderByTitleAsc(email);
+    }
+
+    public Task add(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task update(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteById(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+
+
 }
